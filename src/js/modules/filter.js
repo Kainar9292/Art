2cba62fda5c2cfbf4f050781751bc5590a0d3da5
Kainar-Home <toleubaevkai@mail.ru>
2021-01-14 13:21:40 +0600

    complete filter

--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -1,5 +1,63 @@
 const filter = () => {
-    
+    const menu = document.querySelector('.portfolio-menu'),
+          items = document.querySelectorAll('li'),
+          btnAll = document.querySelector('.all'),
+          btnLovers = menu.querySelector('.lovers'),
+          btnChef = menu.querySelector('.chef'),
+          btnGirl = menu.querySelector('.girl'),
+          btnGuy = menu.querySelector('.guy'),
+          btnGrandmother = menu.querySelector('.grandmother'),
+          btnGranddad = menu.querySelector('.granddad'),
+          wrapper = document.querySelector('.portfolio-wrapper'),
+          markAll = wrapper.querySelectorAll('.all'),
+          markGirl = wrapper.querySelectorAll('.girl'),
+          markLovers = wrapper.querySelectorAll('.lovers'),
+          markChef = wrapper.querySelectorAll('.chef'),
+          markGuy = wrapper.querySelectorAll('.guy'),
+          no = document.querySelector('.portfolio-no');
+
+    const typeFilter = (markType) => {
+        markAll.forEach(mark => {
+            mark.style.display = 'none';
+            mark.classList.remove('animated', 'fadeIn');
+        });
+
+        no.style.display = 'none';
+        no.classList.remove('animated', 'fadeIn');
+
+        if (markType) {
+            markType.forEach(mark => {
+                mark.style.display = 'block';
+                mark.classList.add('animated', 'fadeIn');
+            });
+        } else {
+            no.style.display = 'block';
+            no.classList.add('animated', 'fadeIn');
+        }
+    };
+
+    const btnEvenlListener = (btn, mark) => {
+        btn.addEventListener('click', () => {
+            typeFilter(mark);
+        });
+    };
+
+    btnEvenlListener(btnAll, markAll);
+    btnEvenlListener(btnLovers, markLovers);
+    btnEvenlListener(btnChef, markChef);
+    btnEvenlListener(btnGuy, markGuy);
+    btnEvenlListener(btnGirl, markGirl);
+    btnEvenlListener(btnGrandmother);
+    btnEvenlListener(btnGranddad);
+
+    menu.addEventListener('click', (e) => {
+        let target = e.target;
+
+        if (target && target.tagName == 'LI') {
+            items.forEach(btn => btn.classList.remove('active'));
+            target.classList.add('active');
+        }
+    });
 };
 
 export default filter;
